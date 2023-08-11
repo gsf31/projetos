e92c9b9a5cff5d778886f29599821f1f4d3ce4b4
gsf31 <128832984+gsf31@users.noreply.github.com>
2023-08-11 12:20:25 -0300

    Delete num_inteiro.py

--- num_inteiro.py
+++
@@ -1,7 +0,0 @@
-num_inteiro = input("Digite um número inteiro: ")
-valor_int = int(num_inteiro)
-
-valor_inteiro= (valor_int // 10)
-valor_dezena = (valor_inteiro % 10)
-
-print("O dígito das dezenas é", valor_dezena)
