450cd8e7c0b3e892c3bd3ead29d8d89b95ef7861
Gus <gustavosodrefigueiredo1@gmail.com>
2023-08-11 12:25:12 -0300

    removendo arquvis

--- perim_area.py
+++
@@ -1,8 +0,0 @@
-lado_quadrado = input("Digite o lado do quadrado: ")
-valor_lado = int(lado_quadrado)
-
-perimetro = ((valor_lado + valor_lado + valor_lado + valor_lado))
-area = ((valor_lado * valor_lado))
-
-print("perímetro:", perimetro, "- área:", area)
-
