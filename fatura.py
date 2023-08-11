f705024feb439f35b0cf1291a3160e61ddd5d433
gsf31 <128832984+gsf31@users.noreply.github.com>
2023-08-11 12:19:45 -0300

    Delete fatura.py

--- fatura.py
+++
@@ -1,7 +0,0 @@
-nome_cliente = input("Digite o nome do cliente: ")
-vencimento_cartao = input("Digite o dia do vencimento da sua fatura: ")
-vencimento_mes = input("Digite o mês da sua fatura: ")
-valor_fatura = input("Digite o valor da sua fatura: ")
-
-print("Olá,", nome_cliente)
-print("A sua fatura com vencimento em", vencimento_cartao, "de", vencimento_mes, "no valor de R$", valor_fatura, "está fechada.")
