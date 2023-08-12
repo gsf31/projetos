9c905ab2ac2e5bca358513fb189bc85bd7ba103d
Gus <gustavosodrefigueiredo1@gmail.com>
2023-08-12 16:56:06 -0300

    adicionando uma nova pasta

---
+++ javascript_dev/variaveis.js
@@ -0,0 +1,17 @@
+// Aprendendo sobre variáveis
+
+let idade = 24; // Primeira variável idade
+
+console.log(idade, typeof idade)
+
+// Criando uma nova variável
+let numero = 30;        // Number
+let altura = 1.70;      // Number
+let nome = "Gustavo"    // String
+let estudando = true    // Booleano
+
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
