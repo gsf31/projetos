778f006336a71b9dc07b818e3f4e9ca93986ad25
Gus <gustavosodrefigueiredo1@gmail.com>
2023-08-13 10:40:10 -0300

    aprendendo a fazer variáveis condicionais

---
+++ javascript_dev/condicionais.js
@@ -0,0 +1,24 @@
+const idade = 20;
+maiorDeIdade = 18;
+
+if (idade >= maiorDeIdade) {
+    console.log("Você é maior de idade")
+} else {
+    console.log("Você é menor de idade")
+}
+
+// Média >= 7 (Aprovado)
+// Média < 7 (Reprovado)
+
+const ava1 = 5;
+const ava2 = 3;
+const ava3 = 7;
+
+let media = (ava1 + ava2 + ava3) /  3;
+
+if (media >= 7) {
+    console.log("Sua média foi", media, "você foi Aprovado")
+} else {
+    console.log("Sua média foi", media, "você vai foi Reprovado")
+}
+
---
+++ javascript_dev/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+const idade = 20;
+maiorDeIdade = 18;
+
+if (idade >= maiorDeIdade) {
+    console.log("Você é maior de idade")
+} else {
+    console.log("Você é menor de idade")
+}
