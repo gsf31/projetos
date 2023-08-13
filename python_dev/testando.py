d8e806750a05544230c35e7391646efdb5f1d197
Gus <gustavosodrefigueiredo1@gmail.com>
2023-08-13 10:41:24 -0300

    aprendendo a fazer variáveis condicionais

---
+++ python_dev/testando.py
@@ -0,0 +1,7 @@
+idade = int(input('Digite a sua idade: '))
+podeDirigir = 18
+
+if idade >= podeDirigir:
+    print('Voce tem mais de 18 anos voce ja pode tirar carteira')
+else:
+    print('Voce ainda nao pode dirigir')
